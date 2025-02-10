60356a67790521f2655ec97885f3759c0629611b
kyaukyuai <y.kakui@gmail.com>
2025-02-10 23:04:13 +0900

    feat(agent): add supervisor node and multi-agent support

---
+++ slack_ai_agent/agents/multi_agents/supervisor.py
@@ -0,0 +1,98 @@
+from typing import Literal
+
+from langchain_core.messages import HumanMessage
+from langgraph.graph import END
+from langgraph.graph import START
+from langgraph.graph import MessagesState
+from langgraph.graph import StateGraph
+from langgraph.prebuilt import create_react_agent
+from langgraph.types import Command
+from typing_extensions import TypedDict
+
+from slack_ai_agent.agents.tools.python import create_python_repl_tool
+from slack_ai_agent.agents.tools.search import create_search_tool
+from slack_ai_agent.agents.utils import load_memories
+from slack_ai_agent.agents.utils.models import model
+
+
+members = ["researcher", "coder"]
+# Our team supervisor is an LLM node. It just picks the next agent to process
+# and decides when the work is completed
+options = members + ["FINISH"]
+
+system_prompt = (
+    "You are a supervisor tasked with managing a conversation between the"
+    f" following workers: {members}. Given the following user request,"
+    " respond with the worker to act next. Each worker will perform a"
+    " task and respond with their results and status. When finished,"
+    " respond with FINISH."
+)
+
+
+class Router(TypedDict):
+    """Worker to route to next. If no workers needed, route to FINISH."""
+
+    next: Literal["researcher", "coder", "FINISH"]
+
+
+class State(MessagesState):
+    next: str
+
+
+def supervisor_node(state: State) -> Command[Literal["researcher", "coder", "__end__"]]:
+    messages = [
+        {"role": "system", "content": system_prompt},
+    ] + state["messages"]
+    response = model.with_structured_output(Router).invoke(messages)
+    print("response: ", response)
+    goto = response["next"]
+    if goto == "FINISH":
+        goto = END
+
+    return Command(goto=goto, update={"next": goto})
+
+
+research_agent = create_react_agent(
+    model,
+    tools=[create_search_tool()],
+    prompt="You are a researcher. DO NOT do any math.",
+)
+
+
+def research_node(state: State) -> Command[Literal["supervisor"]]:
+    result = research_agent.invoke(state)
+    return Command(
+        update={
+            "messages": [
+                HumanMessage(content=result["messages"][-1].content, name="researcher")
+            ]
+        },
+        goto="supervisor",
+    )
+
+
+# NOTE: THIS PERFORMS ARBITRARY CODE EXECUTION, WHICH CAN BE UNSAFE WHEN NOT SANDBOXED
+code_agent = create_react_agent(model, tools=[create_python_repl_tool()])
+
+
+def code_node(state: State) -> Command[Literal["supervisor"]]:
+    result = code_agent.invoke(state)
+    return Command(
+        update={
+            "messages": [
+                HumanMessage(content=result["messages"][-1].content, name="coder")
+            ]
+        },
+        goto="supervisor",
+    )
+
+
+builder = StateGraph(State)
+# builder.add_node("load_memories", load_memories)  # type: ignore
+builder.add_node("supervisor", supervisor_node)
+builder.add_node("researcher", research_node)
+builder.add_node("coder", code_node)
+builder.add_edge(START, "supervisor")
+builder.add_edge("supervisor", "researcher")
+builder.add_edge("supervisor", "coder")
+graph = builder.compile()
